6b98bed2eec26b1ce54cd117fb612a07f32f7108
running_pinata <running_pinata>
2005-11-10 20:37:11 +0000

    *** empty log message ***

--- src/converter/char-converter.h
+++
@@ -1,6 +0,0 @@
-#ifndef _CHAR_CONVERTER_H_
-#define _CHAR_CONVERTER_H_
-
-int char_convert(void);
-
-#endif /* _CHAR_CONVERTER_H_ */
--- src/converter/login-converter.h
+++
@@ -1,6 +0,0 @@
-#ifndef _LOGIN_CONVERTER_H_
-#define _LOGIN_CONVERTER_H_
-
-int login_convert(void);
-
-#endif /* _LOGIN_CONVERTER_H_ */
